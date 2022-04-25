f929e73917f6a96441938a9e9b81c2df2dc8e49a
mollerpw <89844600+mollerpw@users.noreply.github.com>
2022-04-25 12:28:23 +0200

    Test save picture

--- src/main/java/com/sprint2/webapi/WebapiApplication.java
+++ src/main/java/com/sprint2/webapi/WebapiApplication.java
@@ -1,9 +1,14 @@
 package com.sprint2.webapi;
 
+import com.sprint2.webapi.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class WebapiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/sprint2/webapi/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.sprint2.webapi.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
